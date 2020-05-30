602487acdc35b0ddb03fa5bdc5c77b36b10ee3a1
moritalous <moritalous@gmail.com>
2020-05-30 22:21:30 +0900

    update

--- aws-iot-greengrass/src/main.cpp
+++ aws-iot-greengrass/src/main.cpp
@@ -4,14 +4,20 @@
 #include <WiFiUdp.h>
 #include <WiFiClientSecure.h>
 #include <AWSGreenGrassIoT.h>
-
 #include <NTPClient.h>
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_AM2320.h>
-
 #include <ArduinoJson.h>
 
 #include "secrets.h"
+
+#include <esp_sleep.h>
+
+#define AWS_IOT_PUBLISH_TOPIC "esp32/am2320"
+
+#define I2C_SDA 33
+#define I2C_SCL 32
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "time1.google.com", 3600, 60000);
@@ -20,13 +26,17 @@ AWSGreenGrassIoT *greengrass;
 
 int status = WL_IDLE_STATUS;
 
-#define AWS_IOT_PUBLISH_TOPIC "sensors/am2320"
-
-#define I2C_SDA 33
-#define I2C_SCL 32
-
 TwoWire myTwoWire = TwoWire(0);
 Adafruit_AM2320 am2320;
+
+int count = 0;
+
+void enterDeepSleep()
+{
+  Serial.println("enter deepSleep");
+  esp_sleep_enable_timer_wakeup(10 * 1000 * 1000);
+  esp_deep_sleep_start();
+}
 
 void publishMessage()
 {
@@ -54,10 +64,6 @@ void setup()
 {
   Serial.begin(9600);
 
-  myTwoWire.begin(I2C_SDA, I2C_SCL, 100000);
-  am2320 = Adafruit_AM2320(&myTwoWire, -1, -1);
-  am2320.begin();
-
   while (status != WL_CONNECTED)
   {
     Serial.print("Attempting to connect to SSID: ");
@@ -65,7 +71,7 @@ void setup()
     // Connect to WPA/WPA2 network.
     status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     // wait 3 seconds for connection:
-    delay(3000);
+    delay(1000);
   }
 
   Serial.println("Connected to wifi");
@@ -74,31 +80,41 @@ void setup()
 
   timeClient.begin();
 
-  if (greengrass->connectToIoTCore() == true)
+  if (greengrass->connectToGG() == true)
   {
-    Serial.println("Connected to AWS IoT core");
+    Serial.println("Connected to AWS IoT Greengrass");
     delay(500);
-    if (greengrass->connectToGG() == true)
-    {
-      Serial.println("Connected to AWS IoT Greengrass");
-      delay(500);
-    }
-    else
-    {
-      Serial.println("Connection to AWS IoT Greengrass failed");
-    }
   }
   else
   {
-    Serial.println("Connection to AWS IoT Core failed");
+    Serial.println("Connection to AWS IoT Greengrass failed");
+    enterDeepSleep();
   }
+
+  myTwoWire.begin(I2C_SDA, I2C_SCL, 100000);
+  pinMode(I2C_SDA, INPUT);
+  pinMode(I2C_SCL, INPUT);
+
+  am2320 = Adafruit_AM2320(&myTwoWire, -1, -1);
+  am2320.begin();
+
+  delay(1000);
 }
 
 void loop()
 {
   if (greengrass->isConnected())
   {
+    count++;
     publishMessage();
+  }
+  else
+  {
+    enterDeepSleep();
+  }
+  if (count > 2000)
+  {
+    enterDeepSleep();
   }
   delay(2000);
 }
